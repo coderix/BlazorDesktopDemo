f6c364cc8b3cec93fed5e2f315de71fb4b99489b
Dirk Neumann <dneumann42@googlemail.com>
2020-10-05 17:54:32 +0200

    menu 1

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorDesktopDemo.Data;
 using ElectronNET.API;
+using ElectronNET.API.Entities;
 
 namespace BlazorDesktopDemo
 {
@@ -64,6 +65,78 @@ namespace BlazorDesktopDemo
         }
         void ElectronBootstrap(){
          Task.Run(async () => await Electron.WindowManager.CreateWindowAsync());
+
+         var menu = new MenuItem[] 
+         {
+            new MenuItem 
+            { 
+                    Label = "Edit", Type = MenuType.submenu, Submenu = new MenuItem[] 
+                    {
+                            new MenuItem { Label = "Undo", Accelerator = "CmdOrCtrl+Z", Role = MenuRole.undo },
+                             new MenuItem { Label = "Redo", Accelerator = "Shift+CmdOrCtrl+Z", Role = MenuRole.redo },
+                             new MenuItem { Type = MenuType.separator },
+                         new MenuItem { Label = "Cut", Accelerator = "CmdOrCtrl+X", Role = MenuRole.cut },
+                         new MenuItem { Label = "Copy", Accelerator = "CmdOrCtrl+C", Role = MenuRole.copy },
+                         new MenuItem { Label = "Paste", Accelerator = "CmdOrCtrl+V", Role = MenuRole.paste },
+                         new MenuItem { Label = "Select All", Accelerator = "CmdOrCtrl+A", Role = MenuRole.selectall }
+                    }
+            },
+            new MenuItem { Label = "Ansicht", Type = MenuType.submenu, Submenu = new MenuItem[] 
+            {
+                 new MenuItem
+                    {
+            Label = "Reload",
+            Accelerator = "CmdOrCtrl+R",
+            Click = () =>
+            {
+                // on reload, start fresh and close any old
+                // open secondary windows
+                Electron.WindowManager.BrowserWindows.ToList().ForEach(browserWindow => {
+                    if(browserWindow.Id != 1)
+                    {
+                        browserWindow.Close();
+                    }
+                    else
+                    {
+                        browserWindow.Reload();
+                    }
+                });
+            }
+        },
+        new MenuItem
+        {
+            Label = "Toggle Full Screen",
+            Accelerator = "CmdOrCtrl+F",
+            Click = async () =>
+            {
+                bool isFullScreen = await Electron.WindowManager.BrowserWindows.First().IsFullScreenAsync();
+                Electron.WindowManager.BrowserWindows.First().SetFullScreen(!isFullScreen);
+            }
+        },
+        new MenuItem
+        {
+            Label = "Open Developer Tools",
+            Accelerator = "CmdOrCtrl+I",
+            Click = () => Electron.WindowManager.BrowserWindows.First().WebContents.OpenDevTools()
+        },
+        
+    }
+    },
+    new MenuItem { Label = "Window", Role = MenuRole.window, Type = MenuType.submenu, Submenu = new MenuItem[] {
+            new MenuItem { Label = "Minimize", Accelerator = "CmdOrCtrl+M", Role = MenuRole.minimize },
+            new MenuItem { Label = "Close", Accelerator = "CmdOrCtrl+W", Role = MenuRole.close },
+            new MenuItem
+             {
+            Label = "Beenden",
+            Accelerator = "CmdOrCtrl+X",
+            Click = () => Electron.App.Exit(0)
+            }
+    }
+    }
+    
+};
+
+Electron.Menu.SetApplicationMenu(menu);
         }
     }
     
