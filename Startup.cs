fe9d14b68a3143212c0e09cdfb94c8f945339d02
Dirk Neumann <dneumann42@googlemail.com>
2020-10-08 15:33:33 +0200

    Menu

--- Startup.cs
+++ Startup.cs
@@ -68,24 +68,38 @@ namespace BlazorDesktopDemo
 
          var menu = new MenuItem[] 
          {
+           new MenuItem
+           { 
+               Label = "Datei", 
+               Role = MenuRole.window, 
+               Type = MenuType.submenu, 
+               Submenu = new MenuItem[] 
+                {
+                    new MenuItem
+                    {
+                        Label = "Beenden",
+                        Click = () => Electron.App.Exit(0)
+                    }
+                }
+            },
             new MenuItem 
             { 
-                    Label = "Edit", Type = MenuType.submenu, Submenu = new MenuItem[] 
+                    Label = "Bearbeiten", Type = MenuType.submenu, Submenu = new MenuItem[] 
                     {
-                            new MenuItem { Label = "Undo", Accelerator = "CmdOrCtrl+Z", Role = MenuRole.undo },
-                             new MenuItem { Label = "Redo", Accelerator = "Shift+CmdOrCtrl+Z", Role = MenuRole.redo },
+                            new MenuItem { Label = "Rückgängig", Accelerator = "CmdOrCtrl+Z", Role = MenuRole.undo },
+                             new MenuItem { Label = "Wiederholen", Accelerator = "Shift+CmdOrCtrl+Z", Role = MenuRole.redo },
                              new MenuItem { Type = MenuType.separator },
-                         new MenuItem { Label = "Cut", Accelerator = "CmdOrCtrl+X", Role = MenuRole.cut },
-                         new MenuItem { Label = "Copy", Accelerator = "CmdOrCtrl+C", Role = MenuRole.copy },
-                         new MenuItem { Label = "Paste", Accelerator = "CmdOrCtrl+V", Role = MenuRole.paste },
-                         new MenuItem { Label = "Select All", Accelerator = "CmdOrCtrl+A", Role = MenuRole.selectall }
+                         new MenuItem { Label = "Ausschneiden", Accelerator = "CmdOrCtrl+X", Role = MenuRole.cut },
+                         new MenuItem { Label = "Kopieren", Accelerator = "CmdOrCtrl+C", Role = MenuRole.copy },
+                         new MenuItem { Label = "Einfügen", Accelerator = "CmdOrCtrl+V", Role = MenuRole.paste },
+                         new MenuItem { Label = "Alles auswählen", Accelerator = "CmdOrCtrl+A", Role = MenuRole.selectall }
                     }
             },
             new MenuItem { Label = "Ansicht", Type = MenuType.submenu, Submenu = new MenuItem[] 
             {
                  new MenuItem
                     {
-            Label = "Reload",
+            Label = "Neu laden",
             Accelerator = "CmdOrCtrl+R",
             Click = () =>
             {
@@ -122,9 +136,9 @@ namespace BlazorDesktopDemo
         
     }
     },
-    new MenuItem { Label = "Window", Role = MenuRole.window, Type = MenuType.submenu, Submenu = new MenuItem[] {
-            new MenuItem { Label = "Minimize", Accelerator = "CmdOrCtrl+M", Role = MenuRole.minimize },
-            new MenuItem { Label = "Close", Accelerator = "CmdOrCtrl+W", Role = MenuRole.close },
+    new MenuItem { Label = "Fenster", Role = MenuRole.window, Type = MenuType.submenu, Submenu = new MenuItem[] {
+            new MenuItem { Label = "Minimieren", Accelerator = "CmdOrCtrl+M", Role = MenuRole.minimize },
+            new MenuItem { Label = "Schließen", Accelerator = "CmdOrCtrl+W", Role = MenuRole.close },
             new MenuItem
              {
             Label = "Beenden",
